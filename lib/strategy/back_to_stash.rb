55a72aa4cecae9e794897580e7285af2eed525b5
Rob Howard <rob@robhoward.id.au>
2012-11-18 18:57:34 +1100

    Fills out back-to-stash strategy.

--- lib/strategy/back_to_stash.rb
+++ lib/strategy/back_to_stash.rb
@@ -9,13 +9,28 @@ module Strategy
 
     def act
       if close_to_stash
+        log('close to stash')
+        random_walk
+      else
+        log('not close to stash')
+        move_to_stash
       end
     end
 
     protected
 
       def close_to_stash
-        false
+        # TODO: We want to make sure we're within two squares' distance.
+        world.you.stash.distance_from(world.you.position) <= 2
+      end
+
+      def random_walk
+        decide 'move', {dir: world.valid_move_directions.sample}
+      end
+
+      def move_to_stash
+        # TODO: Write more complex pather, use it both here and in DropAtStash
+        decide 'move', {dir: world.you.position.direction_from(world.you.stash)} #.direction_from(world.you.position)}
       end
   end
 end
