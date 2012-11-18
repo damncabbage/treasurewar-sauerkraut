dd45b94921819c758c11c4f184c5c8e8748c77bd
Rob Howard <rob@robhoward.id.au>
2012-11-18 18:48:43 +1100

    Adds debug logging.

--- lib/strategy/base.rb
+++ lib/strategy/base.rb
@@ -15,7 +15,16 @@ module Strategy
 
       # Returns ['action name', {action: data}]
       def decide(action, data)
+        log_decision(action, data)
         [action, data]
+      end
+
+      def log(message)
+        Logger.debug self.class.name, message
+      end
+
+      def log_decision(action, data)
+        Logger.debug self.class.name, "#{action} with #{data}"
       end
   end
 end
