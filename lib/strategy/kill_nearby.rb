5f7d658319445f4de40ca1d7a353767a3172d322
Rob Howard <rob@robhoward.id.au>
2012-11-18 18:58:24 +1100

    Moves towards and kills nearby bots.

--- lib/strategy/kill_nearby.rb
+++ lib/strategy/kill_nearby.rb
@@ -5,5 +5,27 @@ module Strategy
     def use?
       world.nearby_players.any?
     end
+    def act
+      player = world.nearby_players.first
+      if next_to_player(player)
+        attack_player(player)
+      else
+        move_to_player(player)
+      end
+    end
+
+    protected
+
+      def next_to_player(player)
+        player.distance_from(world.you.position) < 2
+      end
+
+      def attack_player(player)
+        decide 'attack', {dir: world.you.position.direction_from(player)}
+      end
+
+      def move_to_player(player)
+        decide 'move', {dir: world.you.position.direction_from(player)}
+      end
   end
 end
